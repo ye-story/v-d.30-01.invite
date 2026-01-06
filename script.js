beb3b800066098660b1bf3c8782dc5f79741ab3c
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-01-06 08:05:25 +0300

    feat: load img

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ const id = urlParams.get('id');
 
 const baseImgPath = './img/guests/';
 
+let finalSrc = './img/common.webp'; // Путь по умолчанию
+
 if (+id && !isNaN(+id) && (+id <= 24) && (+id > 0)) { 
- img1.src = baseImgPath + `${id}.webp`; 
+  finalSrc = baseImgPath + `${id}.webp`; 
 }
-else {
-  img1.src = './img/common.webp';
-}
+
+img1.onload = () => {
+  img1.classList.add('loaded');
+};
+
+
+img1.src = finalSrc;
 
 const imgs = [...document.querySelectorAll('.img-wrapper img')];
 
